3d8fd698a7b1db362a52b2a9e2d1fd403ca96733
Polina <ekortika@gmail.com>
2021-05-21 23:13:20 +0300

    Первоначальная ракета

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import shipUrl from "img/ship.png";
+
+const degToRad = (n: number) => (n * Math.PI) / 180;
+
+function drawImage(
+  ctx: CanvasRenderingContext2D,
+  image: HTMLImageElement,
+  x: number,
+  y: number,
+  rotation: number
+) {
+  ctx.setTransform(0.3, 0, 0, 0.3, x, y);
+  ctx.rotate(rotation);
+  ctx.drawImage(image, -image.width / 2, -image.height / 2);
+}
+
+type Coordinates = {
+  x: number;
+  y: number;
+  rotation: number;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+
+  const [coordinates, setCoordinates] = React.useState<Coordinates>({
+    x: 100,
+    y: 100,
+    rotation: 0,
+  });
+
+  useEffect(() => {
+    if (!canvasRef.current) {
+      return;
+    }
+
+    const width = canvasRef.current.width;
+    const height = canvasRef.current.height;
+
+    const ctx = canvasRef.current.getContext("2d");
+
+    if (!ctx) {
+      return;
+    }
+
+    const ship = (state: any) => {
+      //ctx.save();
+      ctx.restore();
+      //ctx.clearRect(100, 100, width, height);
+      const { x, y, rotation } = state;
+      const img = new Image();
+      img.src = shipUrl;
+      img.onload = () => {
+        ctx.drawImage(img, x, y, 100, 100);
+        // drawImage(ctx, img, x, y, rotation);
+        // drawImage(ctx, img, x, y, rotation);
+      };
+    };
+
+    ship(coordinates);
+
+    window.addEventListener("keydown", (e) => {
+      switch (e.code) {
+        case "KeyA": {
+          setCoordinates((prevState) => {
+            const { x } = prevState;
+            const dif = x - 10;
+            return { ...prevState, x: dif };
+          });
+          break;
+        }
+        case "KeyD": {
+          setCoordinates((prevState) => {
+            const { x } = prevState;
+            const dif = x + 10;
+            return { ...prevState, x: dif };
+          });
+          break;
+        }
+        case "KeyW": {
+          setCoordinates((prevState) => {
+            const { y } = prevState;
+            const dif = y - 10;
+            return { ...prevState, y: dif };
+          });
+          break;
+        }
+        case "KeyS": {
+          setCoordinates((prevState) => {
+            const { y } = prevState;
+            const dif = y + 10;
+            return { ...prevState, y: dif };
+          });
+          break;
+        }
+        case "KeySpace": {
+          return;
+        }
+      }
+    });
+  }, [coordinates]);
+
+  return <canvas ref={canvasRef} width="800px" height="800px" />;
 }
 
 export default App;
