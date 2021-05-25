7813984cd5c89e7cdc823c4d0f3961acae8bd5d8
Polina <ekortika@gmail.com>
2021-05-25 16:05:42 +0300

    Слайдер на css + js

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [img, setImg] = React.useState(0);
+
+  const sliderRef = React.useRef<HTMLDivElement>(null);
+
+  const back = () => {
+    changeImg(-1);
+  };
+
+  const next = () => {
+    changeImg(+1);
+  };
+
+  const changeImg = (arg: number) => {
+    setImg((prevState) => {
+      const newImg = prevState + arg;
+      switch (newImg) {
+        case -1: {
+          return 2;
+        }
+        case 3: {
+          return 0;
+        }
+      }
+
+      return newImg;
+    });
+  };
+
+  React.useEffect(() => {
+    if (!sliderRef.current) {
+      return;
+    }
+
+    const width = sliderRef.current.getBoundingClientRect().width;
+    sliderRef.current.scrollLeft = img * width;
+  }, [img]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={back}>Назад </button>
+      <button onClick={next}>Вперед </button>
+
+      <div className="slider" ref={sliderRef}>
+        <img src={`img/1.jpg`} />
+        <img src={`img/2.jpg`} />
+        <img src={`img/3.jpg`} />
+      </div>
     </div>
   );
 }
