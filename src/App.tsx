afd7f023777441710078d2a9fadbfff7877e4ada
Polina <ekortika@gmail.com>
2021-05-27 22:47:32 +0300

    убрала лишние стили, поправила роут

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,33 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Auth from "pages/Auth";
+import Auth from "pages/Auth/Auth";
+import Sungup from "pages/Sungup/Sungup";
+
+const routes = [
+  {
+    path: "/",
+    exact: true,
+    component: Auth,
+  },
+  {
+    path: "/sungup",
+    component: Sungup,
+  },
+];
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/">
-          <Auth />
-        </Route>
+        {routes.map((route, index) => (
+          <Route
+            key={index}
+            path={route.path}
+            exact={route.exact}
+            render={route.component}
+          />
+        ))}
       </Switch>
     </Router>
   );
