69f968174fd370b7a363a86d98d227106a73b6a7
Polina <ekortika@gmail.com>
2021-05-31 16:59:06 +0300

    выносим запрросы к серверу  в отдельный файл

---
+++ src/utils.ts
@@ -0,0 +1,49 @@
+const regUrl = "/user/create";
+const authUrl = "/user/login";
+const dataUrl = "/user/data";
+
+const getFormData = (email: string, password: string) => {
+  const formData = new FormData();
+  formData.append("email", email);
+  formData.append("password", password);
+  return formData;
+};
+
+export const getData = (userId: string) => {
+  const url = dataUrl + `?query=${userId}`;
+
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => error);
+};
+
+export const signIn = (email: string, password: string) => {
+  const params: RequestInit = {
+    method: "POST",
+    headers: {
+      ContentType: "application/json",
+    },
+    body: getFormData(email, password),
+  };
+
+  return fetch(authUrl, params)
+    .then((response) => response.json())
+    .then((json) => json)
+    .catch((error) => error);
+};
+
+export const signUp = (email: string, password: string) => {
+  const params: RequestInit = {
+    method: "POST",
+    headers: {
+      ContentType: "application/json",
+    },
+    body: getFormData(email, password),
+  };
+
+  return fetch(regUrl, params)
+    .then((response) => response.json())
+    .then((json) => json)
+    .catch((error) => error);
+};
