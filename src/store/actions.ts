e794083a75aba086620b0d96c0e87c8084fb8f68
Polina <ekortika@gmail.com>
2021-06-03 14:15:04 +0300

    саги

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,4 +1,14 @@
 import { createAction } from "@reduxjs/toolkit";
 import { User } from "store/types";
 
-export const getUser = createAction<User>("getUser");
+export const authorizationUser =
+  createAction<{ login: string; password: string }>("authorizationUser");
+
+export const setUser = createAction<User>("setUser");
+
+export const registerUser =
+  createAction<{ login: string; password: string }>("registerUser");
+
+export const getUserData = createAction<User>("getUserData");
+
+export const setUserData = createAction<User>("setUserData");
--- src/store/types.ts
+++ src/store/types.ts
@@ -5,3 +5,12 @@ export type User = {
   email: string;
   name: null;
 };
+
+export type Data = {
+  id: number;
+  ds: string;
+  ans: string;
+  wtl: string;
+  userId: string;
+  date: Date;
+};
