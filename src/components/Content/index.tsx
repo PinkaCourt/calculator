e1ee84e920d61bd5c3fe8dd90cf9209da0ccc1a2
Polina <ekortika@gmail.com>
2021-06-04 17:55:26 +0300

    массив виджетов

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -7,14 +7,19 @@ import Chart from "components/Chart";
 import "./Content.css";
 
 const toAverage = (data: string[]) => {
-  const quantity = data.length;
-
+  const { length } = data;
   const sum = data.reduce((acc, value) => acc + parseFloat(value), 0);
-
-  const average = (sum / quantity).toFixed(2);
+  const average = (sum / length).toFixed(2);
 
   return average;
 };
+
+const toNormalize = (value: string) => {
+  return parseFloat(value).toFixed(2);
+};
+
+const startPeriod = new Date().setMonth(new Date().getMonth() - 1);
+const endPeriod = Date.now();
 
 const Content = () => {
   const data = useSelector(selectData);
@@ -23,48 +28,39 @@ const Content = () => {
     return null;
   }
 
-  const startPeriod = new Date().setMonth(new Date().getMonth() - 1);
-  const endPeriod = Date.now();
-
   const monthlyData = data.filter(
     (mnt) => mnt.date > startPeriod && mnt.date < endPeriod
   );
 
-  const averageDS = toAverage(monthlyData.map((e) => e.ds));
-  const averageAS = toAverage(monthlyData.map((e) => e.ans));
-  const averageWL = toAverage(monthlyData.map((e) => e.wtl));
+  const lastDatas = monthlyData[0];
 
-  /*
-  const toWidget = ()=> {
-
-  }*/
-
-  const widgetDS = {
-    name: "DICK SIZE, cm",
-    data: monthlyData[0].ds,
-    average: averageDS,
-  };
-  const widgetAS = {
-    name: "ANAL SIZE, cm",
-    data: monthlyData[0].ans,
-    average: averageAS,
-  };
-
-  const widgetWL = {
-    name: "DICK SIZE, cm",
-    data: monthlyData[0].wtl,
-    average: averageWL,
-  };
-
-  const arr = [widgetDS, widgetAS, widgetWL];
+  const widgets = [
+    {
+      name: "DICK SIZE, cm",
+      data: toNormalize(lastDatas.ds),
+      average: toAverage(monthlyData.map((e) => e.ds)),
+    },
+    {
+      name: "ANAL SIZE, cm",
+      data: toNormalize(lastDatas.ans),
+      average: toAverage(monthlyData.map((e) => e.ans)),
+    },
+    {
+      name: "WILL TO LIVE, %",
+      data: toNormalize(lastDatas.wtl),
+      average: toAverage(monthlyData.map((e) => e.wtl)),
+    },
+  ];
 
   return (
     <div className={"content_container"}>
-      <Widget
-        name={widgetDS.name}
-        data={widgetDS.data}
-        average={widgetDS.average}
-      />
+      {widgets.map((widget) => (
+        <Widget
+          name={widget.name}
+          data={widget.data}
+          average={widget.average}
+        />
+      ))}
       <Chart />
     </div>
   );
