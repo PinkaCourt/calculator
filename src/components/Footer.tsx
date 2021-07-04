62f46293924d52c2e063feb14e12a3c0771e2d12
Polina <ekortika@gmail.com>
2021-07-05 00:38:44 +0300

    убираем лишние скобки

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import "components/Components.css";
 
 const Footer = () => {
   return (
-    <footer className={"footer"}>created with depression © 2021-2021</footer>
+    <footer className="footer">created with depression © 2021-2021</footer>
   );
 };
 
